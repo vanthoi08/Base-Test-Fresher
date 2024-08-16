e4bc92b1174788e97df347af4696311a462e02a2
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-08-16 18:21:27 +0700

    #22 Axios va Axios Customize

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,74 +1,98 @@
-
-import { Button, Col, Form, Input, Row } from 'antd';
+import { Button, Col, Form, Input, Row } from "antd";
 import { Divider } from "antd";
+import "./register.scss";
+import { Link } from "react-router-dom";
 
-const RegisterPage = () =>{
-    const [form] = Form.useForm();
-    const onFinish = (values) =>{
-        console.log(">>> Check  value" ,values);
-    }
-    return(
-        <>
-          <div className="register-page" style={{padding: "50px"}}>
-          <h2 
-              style={{textAlign:"center"}}
+const RegisterPage = () => {
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log(">>> Check  value", values);
+  };
+  return (
+    <>
+      <div className="register-page" >
+        <main className="main">
+          <div className="container">
+            <section className="wrapper">
+              <div className="heading">
+                <h2 className="text text-large">Đăng Ký Tài Khoản</h2>
+                <Divider />
+              </div>
+              <Form
+                name="basic"
+                labelCol={{ span: 6 }}
+                form={form}
+                onFinish={onFinish}
+                autoComplete="off"
+                style={{ maxWidth: 600, margin: "0 auto" }}
               >
-                Đăng ký người dùng mới
-            </h2>
-        <Divider />
-        
-        <Form
-            name="basic"
-            labelCol={{ span: 6 }}
-            form={form}
-            onFinish={onFinish}
-            autoComplete="off"
-            style={{ maxWidth: 600, margin: "0 auto" }}
-         >
-            
+                <Form.Item
+                  labelCol={{ span: 24 }}
+                  label="Full Name :"
+                  name="fullName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "username không được để trống !",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
 
-            <Form.Item
-                label="Full Name :"
-                name="fullName"
-             rules={[{ required: true, message: 'username không được để trống !' }]}
-             >
-                <Input />
-             </Form.Item>
+                <Form.Item
+                  labelCol={{ span: 24 }}
+                  label="Email :"
+                  name="email"
+                  rules={[
+                    { required: true, message: "email không được để trống !" },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
 
-            <Form.Item
-                label="Email :"
-                name="email"
-             rules={[{ required: true, message: 'email không được để trống !' }]}
-             >
-                <Input />
-             </Form.Item>
-      
+                <Form.Item
+                  labelCol={{ span: 24 }}
+                  label="Password :"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "password không được để trống !",
+                    },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
 
-            <Form.Item
-                label="Password :"
-                name="password"
-             rules={[{ required: true, message: 'password không được để trống !' }]}
-             >
-                <Input.Password />
-             </Form.Item>
+                <Form.Item
+                  labelCol={{ span: 24 }}
+                  label="Phone :"
+                  name="phone"
+                  rules={[
+                    { required: true, message: "phone không được để trống !" },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
 
-            <Form.Item
-                label="Phone :"
-                name="phone"
-             rules={[{ required: true, message: 'phone không được để trống !' }]}
-             >
-                <Input />
-             </Form.Item>
-
-             <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-                <Button type="primary" htmlType="submit">
+                <Form.Item >
+                  <Button type="primary" htmlType="submit" loading={false}>
                     Register
-                 </Button>
-            </Form.Item>
-
-  </Form>
-  </div>
-        </>
-    );
-}
+                  </Button>
+                </Form.Item>
+                <Divider>Or</Divider>
+                <p className="text text-normal">Đã có tài khoản ?
+                  <span>
+                     <Link to={"/login"}> Đăng nhập</Link>
+                  </span>
+                </p>
+              </Form>
+            </section>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
 export default RegisterPage;
